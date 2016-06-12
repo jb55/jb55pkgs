c5ce6abb4951ead46f6ac2f6e7a459edf2deba7b
William Casarin <bill@casarin.me>
2016-06-12 16:54:00 -0700

    samp: init at 0.0.3

--- default.nix
+++ default.nix
@@ -1,11 +1,12 @@
 { nixpkgs ? import <nixpkgs> {} }:
 let callPackage = nixpkgs.pkgs.callPackage;
 in rec {
-  dbopen = callPackage ./pkgs/dbopen { };
-  # curlc = callPackage ./pkgs/curlc { };
-  sharefile = callPackage ./pkgs/sharefile { };
-  mandown = callPackage ./pkgs/mandown { };
-  extname = callPackage ./pkgs/extname { };
+  # curlc   = callPackage ./pkgs/curlc { };
   csv-delim = callPackage ./pkgs/csv-delim { };
-  snap = callPackage ./pkgs/snap { inherit sharefile; };
+  dbopen    = callPackage ./pkgs/dbopen { };
+  extname   = callPackage ./pkgs/extname { };
+  mandown   = callPackage ./pkgs/mandown { };
+  snap      = callPackage ./pkgs/snap { inherit sharefile; };
+  sharefile = callPackage ./pkgs/sharefile { };
+  samp      = callPackage ./pkgs/samp { };
 }
