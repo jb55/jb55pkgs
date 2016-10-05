7f264661fdec478b9d9b179e2bc6d283e46928d3
William Casarin <bill@casarin.me>
2016-10-05 14:08:54 -0700

    csv-scripts: fix csv-sum

--- pkgs/csv-scripts/default.nix
+++ pkgs/csv-scripts/default.nix
@@ -1,24 +1,26 @@
 { stdenv
 , fetchFromGitHub
 , csv-delim
+, bc
 }:
 stdenv.mkDerivation rec {
   name = "csv-scripts-${version}";
-  version = "0.2.3";
+  version = "0.2.5";
 
-  buildInputs = [ csv-delim ];
+  buildInputs = [ csv-delim bc ];
 
   src = fetchFromGitHub {
     owner = "jb55";
     repo = "csv-scripts";
     rev = version;
-    sha256 = "0lw1x2cajam38d49p5cm8l35jqkhz034zimbh1wycly434cr16yg";
+    sha256 = "0w4sldalnfxyrkfqsr6spp7s3fqrjlxjxbq7lzpqb0iqacsw3ali";
   };
 
   patchPhase = ''
     for file in "bin/"*; do
       sed -i 's,csv-delim,${csv-delim}/bin/csv-delim,g' "$file"
     done;
+    sed -i 's,bc,${bc}/bin/bc,' bin/csv-sum
   '';
 
   makeFlags = "PREFIX=$(out)";
