b096f81f2d217905f541b76d4534de82caf8ca8d
William Casarin <jb55@jb55.com>
2019-06-01 18:08:07 -0700

    add bcalc

--- default.nix
+++ default.nix
@@ -1,6 +1,9 @@
 { nixpkgs ? import <nixpkgs> {} }:
 let callPackage = nixpkgs.pkgs.callPackage;
     callHsPackage = nixpkgs.pkgs.haskellPackages.callPackage;
+    fetchGitHub = rest: callPackage (nixpkgs.pkgs.fetchFromGitHub ({
+      owner = "jb55";
+    } // rest)) {};
 in rec {
   curlc       = callPackage ./pkgs/curlc {};
   csv-delim   = callPackage ./pkgs/csv-delim { };
@@ -12,5 +15,12 @@ in rec {
   sharefile   = callPackage ./pkgs/sharefile { };
   samp        = callPackage ./pkgs/samp { };
   ratio       = callPackage ./pkgs/ratio { };
+
+  bcalc       = fetchGitHub {
+                  repo   = "bcalc";
+                  rev    = "d4cc28f367ff6e43d51afc8b4be018aa7385cf86";
+                  sha256 = "02ns6kfnp5g10m2ivhkspp1iws511bcxzw2455ny38hq14f8q1am";
+                };
+
   hearpress   = callHsPackage ./pkgs/hearpress { };
 }
