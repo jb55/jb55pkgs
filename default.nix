3742fa1e1220d54007cc16acf28e2f32ad01b387
William Casarin <jb55@jb55.com>
2019-09-29 03:37:45 -0700

    update snap, add btcs

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
-{ pkgs ? (import <nixpkgs> {}).pkgs }:
+{ pkgs ? import <nixpkgs> {} }:
 let callPackage = pkgs.callPackage;
     callHsPackage = pkgs.haskellPackages.callPackage;
-    fetchGitHub = rest: args: callPackage (pkgs.fetchFromGitHub ({
+    fetch = rest: args: callPackage (pkgs.fetchFromGitHub ({
       owner = "jb55";
     } // rest)) args;
 in rec {
@@ -11,16 +11,27 @@ in rec {
   dbopen      = callPackage ./pkgs/dbopen { };
   extname     = callPackage ./pkgs/extname { };
   mandown     = callPackage ./pkgs/mandown { };
-  snap        = callPackage ./pkgs/snap { inherit sharefile; };
   sharefile   = callPackage ./pkgs/sharefile { };
   samp        = callPackage ./pkgs/samp { };
   ratio       = callPackage ./pkgs/ratio { };
 
-  bcalc       = fetchGitHub {
-                  repo   = "bcalc";
-                  rev    = "5a51083ec33883d3fec3c74cb0891b317f4d1f35";
-                  sha256 = "1mdkpd8rc5y4a4j0mwv7mkfd58a43mpxkxhrjlkkmcf2nngvqi0j";
-                } {};
+  btcs = fetch {
+    repo = "btcs";
+    rev  = "0.1";
+    sha256 = "1ls4wr7ii6icr43z3n49pm1z11sdxv06g799ww8pvxv1ax7aysl6";
+  } {};
+
+  snap = fetch {
+    repo = "sharefile-snap";
+    rev  = "1.3";
+    sha256 = "0j8j2588z09v7zz1f9d11zw2n0wq70sxy9lvwrw4l5yz75n3cral";
+  } {};
+
+  bcalc = fetch {
+    repo   = "bcalc";
+    rev    = "5a51083ec33883d3fec3c74cb0891b317f4d1f35";
+    sha256 = "1mdkpd8rc5y4a4j0mwv7mkfd58a43mpxkxhrjlkkmcf2nngvqi0j";
+  } {};
 
   hearpress   = callHsPackage ./pkgs/hearpress { };
 }
