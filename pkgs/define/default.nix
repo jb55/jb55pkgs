8631f9ad35b0b5274b2542eae5c93ec8c75d4ea0
William Casarin <jb55@jb55.com>
2021-01-02 10:26:43 -0800

    citadel sync

---
+++ pkgs/define/default.nix
@@ -0,0 +1,36 @@
+{ pkgs ? import <nixpkgs> {}, stdenv ? pkgs.stdenv, fetchurl ? pkgs.fetchurl, 
+  bash ? pkgs.bash, less ? pkgs.less, sdcv ? pkgs.sdcv, gnutar ? pkgs.gnutar, 
+  writeScript ? pkgs.writeScript }:
+let
+  dictd = stdenv.mkDerivation rec {
+    pname = "define-dict";
+    version = "0.1";
+
+    src = fetchurl {
+      url = "https://jb55.com/files/dict.tar";
+      sha256 = "fcaf90833b777a1fd9e62282deb245ad62f6ce536f78c6ef503c2d78c3084ab3";
+    };
+
+    installPhase = ''
+      mkdir -p $out/share
+      ${gnutar}/bin/tar xvf $src -C $out/share
+    '';
+  };
+in
+stdenv.mkDerivation rec {
+  pname = "define";
+  version = "0.1";
+
+  src = writeScript "define" ''
+    #!${bash}/bin/bash
+    export LESS="$LESS --quit-if-one-screen"
+    ${sdcv}/bin/sdcv -n --data-dir=${dictd}/share "$@"
+  '';
+
+  phases = ["installPhase"];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src $out/bin/define
+  '';
+}
